fbe6e8b7d2cf2af08e1cbaa4a85a025e9530aea7
Etienne Lacazedieu <elacazedieu@atlassian.com>
2023-12-15 19:07:56 +0100

    Day 13 part 2

--- src/main/java/fr/ela/aoc2023/D13.java
+++ src/main/java/fr/ela/aoc2023/D13.java
@@ -2,6 +2,7 @@ package fr.ela.aoc2023;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiPredicate;
 import java.util.stream.IntStream;
 
 public class D13 extends AoC {
@@ -50,10 +51,54 @@ public class D13 extends AoC {
             return rotate().findReflection();
         }
 
+        public Reflection findFudgeReflection() {
+            for (int i = 1; i < lines.size(); i++) {
+                final int index = i;
+                FudgeMatcher fm = new FudgeMatcher(index);
+                boolean symetry = IntStream.range(0, Math.min(index, lines.size() - index))
+                        .allMatch(offset -> fm.test(offset, this));
+
+                if (symetry && fm.foundFudge) {
+                    return new Reflection(axis, i);
+                }
+            }
+            if (axis == Axis.VERTICAL) {
+                throw new IllegalArgumentException("No symetry");
+            }
+            return rotate().findFudgeReflection();
+        }
+
         boolean matches(int index, int offset) {
             return lines.get(index - offset - 1).equals(lines.get(index + offset));
         }
 
+        boolean hasFudge(int index, int offset) {
+            String one = lines.get(index - offset - 1);
+            String two = lines.get(index + offset);
+            return IntStream.range(0, one.length()).filter(i -> one.charAt(i) != two.charAt(i)).count() == 1;
+        }
+
+    }
+
+    static class FudgeMatcher implements BiPredicate<Integer, LandPattern> {
+        boolean foundFudge = false;
+        final int base;
+
+        FudgeMatcher(int base) {
+            this.base = base;
+        }
+
+        @Override
+        public boolean test(Integer integer, LandPattern landPattern) {
+            if (landPattern.matches(base, integer)) {
+                return true;
+            }
+            if (! foundFudge && landPattern.hasFudge(base, integer)) {
+                foundFudge = true;
+                return true;
+            }
+            return false;
+        }
     }
 
     @Override
@@ -61,11 +106,14 @@ public class D13 extends AoC {
         List<LandPattern> testPatterns = splitOnEmptyLines(getTestInputPath()).stream().map(LandPattern::new).toList();
         long testScore = testPatterns.stream().map(LandPattern::findReflection).mapToLong(Reflection::score).sum();
         System.out.println("Test score part 1 (405) : "+testScore);
-
+        long testScore2 = testPatterns.stream().map(LandPattern::findFudgeReflection).mapToLong(Reflection::score).sum();
+        System.out.println("Test score part 1 (400) : "+testScore2);
 
         List<LandPattern> patterns = splitOnEmptyLines(getInputPath()).stream().map(LandPattern::new).toList();
         long score = patterns.stream().map(LandPattern::findReflection).mapToLong(Reflection::score).sum();
         System.out.println("Score part 1 (27300) : "+score);
+        long score2 = patterns.stream().map(LandPattern::findFudgeReflection).mapToLong(Reflection::score).sum();
+        System.out.println("Score part 2 (29276) : "+score2);
     }
 
 
